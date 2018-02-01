1f888e7d07db5ee134d490cd6c8dd61f8cb04a3e
Pavel Roman <romansp2@gmail.com>
2018-02-01 08:47:23 +0300

    Version1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+
+  resources :fuels
+  root 'fuels#index'
+  get 'fuels/index'
+
+  get 'fuels/new'
+  get 'fuels/create'
+  get 'fuels/edit'
+  delete 'fuels/destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
